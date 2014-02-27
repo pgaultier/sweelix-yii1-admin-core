4a4ad15c0e4496f064d633c9feaf5537367e7803
Philippe Gaultier <pgaultier@sweelix.net>
2014-02-27 14:47:18 +0100

    Fix doc block

--- assets/js/jquery.sweelix.sweeft.js
+++ assets/js/jquery.sweelix.sweeft.js
@@ -4,12 +4,12 @@
  * This is a simple ajax helper
  *
  * @author    Philippe Gaultier <pgaultier@sweelix.net>
- * @copyright 2010-2011 Sweelix
+ * @copyright 2010-2014 Sweelix
  * @license   http://www.sweelix.net/license license
- * @version   XXX
+ * @version   2.0.0
  * @link      http://www.sweelix.net
- * @category  javascript
- * @package   Sweelix.javascript
+ * @category  js
+ * @package   sweelix.yii1.admin.core.assets.js
  */
 (function($) {
 	var module = {
